176cac0b8ccb66fb128e6aa44bcd77889c1fcc71
FravinCustodianStudent <91590998+FravinCustodianStudent@users.noreply.github.com>
2022-07-30 12:47:08 +0300

    Initial commit

---
+++ Patterns/Builder/HTMLElement_Builder.cs
@@ -0,0 +1,43 @@
+﻿namespace Patterns;
+
+public class HtmlBuilder
+{
+    private readonly string rootName;
+    protected HtmlElement root = new HtmlElement();
+
+    public HtmlBuilder(string rootName)
+    {
+        this.rootName = rootName;
+        root.Name = rootName;
+    }
+
+    // not fluent
+    public void AddChild(string childName, string childText)
+    {
+        var e = new HtmlElement(childName, childText);
+        root.Elements.Add(e);
+    }
+
+    public HtmlBuilder AddChildFluent(string childName, string childText)
+    {
+        var e = new HtmlElement(childName, childText);
+        root.Elements.Add(e);
+        return this;
+    }
+
+    public override string ToString() => root.ToString();
+
+    public void Clear()
+    {
+        root = new HtmlElement{Name = rootName};
+    }
+
+    public HtmlElement Build() => root;
+
+    
+    
+    public static implicit operator HtmlElement(HtmlBuilder builder)
+    {
+        return builder.root;
+    }
+}
---
+++ Patterns/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+using Patterns;
+
+var builder = HtmlElement.Create("ul")
+    .AddChildFluent("li","pizdez")
+    .AddChildFluent("Yob","PIZDEZ2")
+    .Build();
+    
