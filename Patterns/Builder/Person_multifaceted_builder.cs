f3cba66234b858537c60b14fc0d942547325a55a
FravinCustodianStudent <91590998+FravinCustodianStudent@users.noreply.github.com>
2022-08-06 16:18:38 +0300

    Builder

---
+++ Patterns/Builder/Person_multifaceted_builder.cs
@@ -0,0 +1,81 @@
+﻿namespace Patterns;
+
+public class Person
+{
+    //address
+    public string StreetAddress, Postcode, City;
+    
+    //employment info
+    public string CompanyName, Position;
+    public int AnnualIncome;
+
+    public override string ToString()
+    {
+        return $"{nameof(StreetAddress)}: {StreetAddress}, {nameof(Postcode)}: {Postcode},\n" +
+               $"{nameof(City)}: {City}, {nameof(CompanyName)}: {CompanyName},\n" +
+               $"{nameof(Position)}: {Position}, {nameof(AnnualIncome)}: {AnnualIncome}";
+    }
+}
+
+public class PersonBuilder // facade 
+{
+    // the object we're going to build
+    protected Person person;
+
+    public PersonBuilder() => person = new Person();
+    protected PersonBuilder(Person person) => this.person = person;
+
+    public PersonAddressBuilder Lives => new PersonAddressBuilder(person);
+    public PersonJobBuilder Works => new PersonJobBuilder(person);
+  
+    public static implicit operator Person(PersonBuilder pb)
+    {
+        return pb.person;
+    }
+}
+
+public class PersonJobBuilder : PersonBuilder
+{
+    public PersonJobBuilder(Person person) : base(person) {}
+
+    public PersonJobBuilder At(string companyName)
+    {
+        person.CompanyName = companyName;
+        return this;
+    }
+
+    public PersonJobBuilder AsA(string position)
+    {
+        person.Position = position;
+        return this;
+    }
+
+    public PersonJobBuilder Earning(int annualIncome)
+    {
+        person.AnnualIncome = annualIncome;
+        return this;
+    }
+}
+
+public class PersonAddressBuilder : PersonBuilder
+{
+    public PersonAddressBuilder(Person person) : base(person) {}
+  
+    public PersonAddressBuilder At(string streetAddress)
+    {
+        person.StreetAddress = streetAddress;
+        return this;
+    }
+  
+    public PersonAddressBuilder WithPostcode(string postcode)
+    {
+        person.Postcode = postcode;
+        return this;
+    }
+  
+    public PersonAddressBuilder In(string city)
+    {
+        person.City = city;
+        return this;
+    }
+}
--- Patterns/Program.cs
+++ Patterns/Program.cs
@@ -7,4 +7,16 @@ var builder = HtmlElement.Create("ul")
     .AddChildFluent("li","pizdez")
     .AddChildFluent("Yob","PIZDEZ2")
     .Build();
+    Console.WriteLine(builder);
     
+var pb = new PersonBuilder();
+Person person = pb
+    .Lives
+        .At("Losharovo 123 road")
+        .In("London")
+        .WithPostcode("SW12BC")
+    .Works
+        .At("fabricam")
+        .AsA("engineer")
+        .Earning(12300);
+Console.WriteLine(person);
