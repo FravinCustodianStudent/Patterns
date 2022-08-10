74a570ca260039cc9de08528327f0d526686907c
FravinCustodianStudent <91590998+FravinCustodianStudent@users.noreply.github.com>
2022-08-10 15:58:12 +0300

    Adapter

--- Patterns/Program.cs
+++ Patterns/Program.cs
@@ -1,13 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using MoreLinq.Extensions;
 using Patterns;
+using Patterns.Adapter;
 using Patterns.Builder;
 using Patterns.Builder.Email_BuilderParametr;
 using Patterns.Factory;
 using Patterns.Factory.Class;
 using Patterns.Prototype.Factory;
 using Person = Patterns.Person;
-using Point = Patterns.Factory.Point;
+using Point = Patterns.Adapter.Point;
 
 void BuilderExamples(){
     var builder = HtmlElement.Create("ul")
@@ -39,31 +41,86 @@ void BuilderExamples(){
     Console.WriteLine(cb.ToString());
 }
 
-void FabricsExamples()
-{
-    var p = Point.NewCartesianPoint(1, 3);
-    var p2 = PointFabric.NewCartesianPoint(1, 3);
-
-    var f = new PersonFactory();
-    var u = f.CreatePerson("Pidor");
-    Console.WriteLine(f.CurrentMaxId+ " " + u.Id);
-
-}
+// void FabricsExamples()
+// {
+//     var p = Point.NewCartesianPoint(1, 3);
+//     var p2 = PointFabric.NewCartesianPoint(1, 3);
+//
+//     var f = new PersonFactory();
+//     var u = f.CreatePerson("Pidor");
+//     Console.WriteLine(f.CurrentMaxId+ " " + u.Id);
+//
+// }
 //FabricsExamples();
-void PrototypeExamples()
+// void PrototypeExamples()
+// {
+//     var main = new Employee(null, new Address("123 East Dr", "London", 0));
+//     var aux = new Employee(null, new Address("123B East Dr", "London", 0));
+//       
+//       
+//     var john = new Employee("John", new Address("123 London Road", "London", 123));
+//
+//     //var chris = john;
+//     var jane = new Employee(john);
+//
+//     jane.Name = "Jane";
+//       
+//     Console.WriteLine(john); // oop s, john is called chris
+//     Console.WriteLine(jane);
+// }
+// PrototypeExamples();
+
+void AdapterExamples()
 {
-    var main = new Employee(null, new Address("123 East Dr", "London", 0));
-    var aux = new Employee(null, new Address("123B East Dr", "London", 0));
-      
-      
-    var john = new Employee("John", new Address("123 London Road", "London", 123));
+    
+ List<VectorObject> vectorObjects 
+    = new List<VectorObject>
+    {
+        new VectorRectangle(1, 1, 10, 10),
+        new VectorRectangle(3, 3, 6, 6)
+    };
 
-    //var chris = john;
-    var jane = new Employee(john);
-
-    jane.Name = "Jane";
-      
-    Console.WriteLine(john); // oop s, john is called chris
-    Console.WriteLine(jane);
+// the interface we have
+void DrawPoint(Point p)
+{
+    Console.Write(".");
 }
-PrototypeExamples();
+
+
+  List<Point> points = new List<Point>();
+bool prepared = false;
+void Prepare()
+{
+    if (prepared) return;
+    foreach (var vo in vectorObjects)
+    {
+        foreach (var line in vo)
+        {
+            var adapter = new LineToPointAdapter(line);
+            adapter.ForEach(p => points.Add(p));
+        }
+    }
+    prepared = true;
+}
+
+void DrawPointsLazy()
+{
+    Prepare();
+    points.ForEach(DrawPoint);
+}
+
+void DrawPoints()
+{
+    foreach (var vo in vectorObjects)
+    {
+        foreach (var line in vo)
+        {
+            var adapter = new LineToPointAdapter(line);
+            adapter.ForEach(DrawPoint);
+        }
+    }
+}
+DrawPoints();
+DrawPoints();
+}
+AdapterExamples();
